a3af89f77c6cd4ef233f29faeb2c133a0b59c695
jiyoonhur <jiyoon1156@gmail.com>
2020-08-30 19:48:43 +0900

    FIX: remove unused file

--- 4_libasm/main.c
+++ 4_libasm/main.c
@@ -1,51 +1,55 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: jhur <marvin@42.fr>                        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/05/18 14:38:56 by jhur              #+#    #+#             */
-/*   Updated: 2020/07/12 13:08:26 by jhur             ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include "libasm.h"
 
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <errno.h>
-
-// size_t	ft_strlen(const char *s);
-// size_t ft_strcpy(char *dst, const char *src);
-// ssize_t	ft_write(int fd, void const *buf, size_t nbyte);
-// int ft_strcmp(const char *s1, const char *s2);
-char *ft_strdup(const char *s1);
-
-int main()
+int main(void)
 {
-	char *s = "hell";
-	char *d = "hello";
-	char *c = "";
-	// char *b;
-	char *aa;
-	char *bb;
-	char *cc;
-	//printf("%zu\n", ft_strlen(s));
-	//ft_strcpy(d, s);
-	//printf("%s\n", d);
+	char dest[50];
+	char buf[10];
+	int fd;
+	char *a;
+	char *b;
+	char *c;
 
-	//printf("%d\n", ft_strcmp(d, s));
-	//printf("%d\n", strcmp(d, s));
-	//printf("%d", strcmp(d, s));
-	//ft_write(1, s, 4);
-	//printf("%zd\n", ft_write(1, c, 4));
-	//printf("%d\n", errno);
-	//printf("%zd\n", write(1, c, 4));
-	//printf("%d\n", errno);
-	//ft_write(1, s, 4);
-	//write(1, s, 4);
-	char *b = ft_strdup("hello");
-	char *str = ft_strdup("hey");
-	printf("%s\n%s\n", b, str);
+	printf("========ft_write=======\n");
+	printf("ft_write_size is : %zu\n", ft_write(1, "hello\n", 6));
+	printf("errno %d\n", errno);
+	ft_write(3, "hello\n", 6);
+	printf("errno %d\n", errno);
+	printf("========ft_read=======\n");
+	fd = open("test.txt", O_RDONLY);
+	printf("ft_read_size is : %zu\n", ft_read(fd, buf, 5));
+	buf[5] = '\n';
+	buf[6] = '\0';
+	ft_write(1, buf, 6);
+	printf("errno %d\n", errno);
+	fd = open("test2.txt", O_RDONLY);
+	ft_read(fd, buf, -1);
+	buf[5] = '\n';
+	buf[6] = '\0';
+	ft_write(1, buf, 6);
+	printf("errno %d\n", errno);
+	printf("========ft_strlen=======\n");
+	printf("strlen is : %zu\n", ft_strlen("wow"));
+	printf("strlen is : %zu\n", ft_strlen(""));
+	printf("========ft_strcpy=======\n");
+	printf("%s\n", ft_strcpy(dest, "licorne"));
+	printf("%s\n", ft_strcpy(dest, "aaa"));
+	printf("%s\n", ft_strcpy(dest, "aa"));
+	printf("%s\n", ft_strcpy(dest, ""));
+	printf("========ft_strcmp=======\n");
+	printf("%d\n", ft_strcmp("abc", "abc"));
+	printf("%d\n", ft_strcmp("a", "b"));
+	printf("%d\n", ft_strcmp("", "a"));
+	printf("%d\n", ft_strcmp("aaaaa", "a"));
+	printf("%d\n", ft_strcmp("a", "aaaaa"));
+	printf("========ft_strdup=======\n");
+	a = ft_strdup("hello");
+	b = ft_strdup("strdup is done!");
+	c = ft_strdup("");
+	printf("%s\n", a);
+	printf("%s\n", b);
+	printf("%s\n", c);
+	free(a);
+	free(b);
+	free(c);
+	return (0);
 }
